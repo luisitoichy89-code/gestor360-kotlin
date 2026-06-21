dd73ee7bfe1943ba1de67accaafc66f894188834
luisitoichy89-code <luisitoichy89@gmail.com>
2026-06-21 10:50:52 -0400

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file - configuración global del proyecto
+//
+// Combinación de versiones VERIFICADA como compatible entre sí
+// (AGP 8.7.x + Kotlin 1.9.25 + Compose compiler 1.5.15 + Gradle 8.9).
+plugins {
+    id("com.android.application") version "8.7.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.25" apply false
+}
