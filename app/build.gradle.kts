ae5117cef77c60a73f0a7a82a134148d3923f93f
luisitoichy89-code <luisitoichy89@gmail.com>
2026-06-21 14:55:28 -0400

    Corregir jvmTarget a kotlinOptions válido para Kotlin 2.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs += "-Xskip-metadata-version-check"
     }
 
     packaging {
