915d192daef3e3254c6926348607cec14910b8e3
luisitoichy89-code <luisitoichy89@gmail.com>
2026-06-22 15:25:38 -0400

    Fix: KSP actualizado a 2.3.9 (compatible con Kotlin 2.3.21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
-    id("com.google.devtools.ksp") version "2.3.21-1.0.31"
+    id("com.google.devtools.ksp") version "2.3.9"
 }
 
 android {
