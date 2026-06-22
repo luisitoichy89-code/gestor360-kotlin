54138f46cea57cd8eba8874afc6872669156b510
luisitoichy89-code <luisitoichy89@gmail.com>
2026-06-22 18:40:05 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,8 @@
 // Desde Kotlin 2.0+, el Compose Compiler se mueve a su propio plugin
 // (org.jetbrains.kotlin.plugin.compose), con la misma versión que Kotlin.
 plugins {
-    id("com.android.application") version "8.7.2" apply false
+    id("com.android.application") version "8.12.0" apply false
     id("org.jetbrains.kotlin.android") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
-}
-
-repositories {
-    maven { url = uri("https://mirrors.aliyun.com/google/") }
-    maven { url = uri("https://mirrors.aliyun.com/repository/public/") }
-    mavenCentral()
 }
