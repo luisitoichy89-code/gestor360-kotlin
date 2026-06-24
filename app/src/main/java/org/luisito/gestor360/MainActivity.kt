aab92c8fe6e390ed7243f28e4c665c9de4db9c6c
luisitoichy89-code <luisitoichy89@gmail.com>
2026-06-23 22:49:07 -0400

    Agregar menú lateral y navegación básica

--- app/src/main/java/org/luisito/gestor360/MainActivity.kt
+++ app/src/main/java/org/luisito/gestor360/MainActivity.kt
@@ -3,6 +3,8 @@ package org.luisito.gestor360
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -10,11 +12,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewmodel.compose.viewModel
+import org.luisito.gestor360.ui.components.Gestor360Drawer
 import org.luisito.gestor360.ui.screens.DashboardScreen
 import org.luisito.gestor360.ui.screens.activation.ActivationScreen
 import org.luisito.gestor360.ui.screens.login.LoginScreen
 import org.luisito.gestor360.ui.screens.login.LoginViewModel
 import org.luisito.gestor360.ui.theme.Gestor360Theme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +34,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Gestor360App() {
     var isLicensed by remember { mutableStateOf(true) }
+    var selectedItem by remember { mutableStateOf("dashboard") }
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
     val loginViewModel: LoginViewModel = viewModel()
     val loginState by loginViewModel.uiState.collectAsState()
@@ -46,13 +53,28 @@ fun Gestor360App() {
             error = loginState.error
         )
     } else {
-        // Mostrar el rol en el Dashboard
-        DashboardScreen(
-            userRol = loginState.userRol,
-            username = loginState.username,
-            onLogout = {
-                loginViewModel.resetState()
+        Gestor360Drawer(
+            drawerState = drawerState,
+            selectedItem = selectedItem,
+            onItemClick = { item ->
+                selectedItem = item
+                when (item) {
+                    "logout" -> loginViewModel.resetState()
+                }
             }
-        )
+        ) {
+            DashboardScreen(
+                userRol = loginState.userRol,
+                username = loginState.username,
+                onMenuClick = {
+                    CoroutineScope(it).launch {
+                        drawerState.open()
+                    }
+                },
+                onLogout = {
+                    loginViewModel.resetState()
+                }
+            )
+        }
     }
 }
