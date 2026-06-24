c5ef1740a80b78463d2c3f014f0d26edba04e24a
luisitoichy89-code <luisitoichy89@gmail.com>
2026-06-24 03:25:41 -0400

    Restaurar todos los archivos a la versión estable que funcionaba

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("kotlin-kapt")
 }
 
