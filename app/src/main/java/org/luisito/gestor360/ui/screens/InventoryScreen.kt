08566a8c81940cade8d7fa8603b95fdf62f0f525
luisitoichy89-code <luisitoichy89@gmail.com>
2026-06-24 00:06:42 -0400

    Corregir imports y referencias en Inventory, Merma, Traces y SalesScreen

--- app/src/main/java/org/luisito/gestor360/ui/screens/InventoryScreen.kt
+++ app/src/main/java/org/luisito/gestor360/ui/screens/InventoryScreen.kt
@@ -3,12 +3,14 @@ package org.luisito.gestor360.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
--- app/src/main/java/org/luisito/gestor360/ui/screens/TracesScreen.kt
+++ app/src/main/java/org/luisito/gestor360/ui/screens/TracesScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
